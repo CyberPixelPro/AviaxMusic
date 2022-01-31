43bdb7a0c78eb6951416c64318d3a2332a1072b8
TechShreyash <shreyashdheemar123@gmail.com>
2022-01-31 21:32:37 +0530

    Some Fixes

--- Yukki/__init__.py
+++ Yukki/__init__.py
@@ -89,13 +89,7 @@ ASSMENTION5 = ""
 random_assistant = []
 
 
-async def initiate_bot():
-    if BOT_USERNAME != "SiestaXMusicBot":
-        print("You cant use this bot. Go away.")
-        return
-    elif BOT_USERNAME != "Kaori_Music_Bot":
-        print("You cant use this bot. Go away.")
-        return
+async def initiate_bot():    
     global SUDOERS, OWNER_ID, ASSIDS
     global BOT_ID, BOT_NAME
     global ASSID1, ASSNAME1, ASSMENTION1, ASSUSERNAME1
