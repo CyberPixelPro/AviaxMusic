ed0fc56926b2a5577451278a9672da2c61734228
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-02-28 21:11:57 +0530

    Decreased Background Image Blur

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -44,7 +44,7 @@ async def gen_thumb(thumbnail, title, userid, status, views, duration, channel):
     black = Image.open("Utils/black.jpg")
     circle = Image.open("Utils/circle.png")
     image1 = changeImageSize(1280, 720, image)
-    image1 = image1.filter(ImageFilter.BoxBlur(20))
+    image1 = image1.filter(ImageFilter.BoxBlur(10))
     image2 = Image.blend(image1,black,0.6)
 
     # Cropping circle from thubnail
