c852394788088b4559f89462dd6c13970ef59bd3
AT WORLD'S END <87578989+AT-WORLDS-END@users.noreply.github.com>
2022-03-04 17:59:12 +0530

    Update Stats.py

--- Yukki/Plugins/Stats.py
+++ Yukki/Plugins/Stats.py
@@ -60,7 +60,7 @@ async def gstats(_, message):
         pass
     uptime = await bot_sys_stats()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg", caption="Getting Stats!"
+        photo="Utils/Aviax.jpg", caption="Getting Stats!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
