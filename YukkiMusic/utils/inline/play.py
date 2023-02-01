819bd40cbaacc4d15b0064de157a18d879181d6a
Musicharshiu <124171749+Musicharshiu@users.noreply.github.com>
2023-02-01 20:06:46 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text="🏠 Support",
-                url="https://telegram.dog/AviaxSupport",
+                url="https://t.me/chatting_gruap",
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"],
@@ -123,7 +123,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                 text="🏠 Support",
-                url="https://telegram.dog/AviaxSupport",
+                url="https://t.me/chatting_gruap",
             ),
             InlineKeyboardButton(
                 text=_["PL_B_3"],
