dde3ea1542683001211abb9e1cee0fe32bbf8fdf
Mr Ravi Official <116261700+ravindu-sathsara@users.noreply.github.com>
2024-08-27 10:32:56 +0530

    Update privacy.py

--- AviaxMusic/plugins/bot/privacy.py
+++ AviaxMusic/plugins/bot/privacy.py
@@ -9,7 +9,7 @@ TEXT = f"""
 
 Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy]({config.PRIVACY_LINK}).
 
-If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/NexGenSpam).
+If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/Inmune_X_IRMusicBotgroup).
 """
 
 @app.on_message(filters.command("privacy"))
