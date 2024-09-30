e0d709da51ce17cda6323c5392bace9878dbc29d
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-09-30 13:33:56 +0530

    Added MassAction ++

--- AviaxMusic/utils/inline/help.py
+++ AviaxMusic/utils/inline/help.py
@@ -86,6 +86,12 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb15",
                 ),
             ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_16"],
+                    callback_data="help_callback hb16",
+                ),
+            ],
             mark,
         ]
     )
