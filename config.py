3b693a91eaa0f6a6d2220bdb8cffcb0a1341fa78
Rahul Kumar <87578989+CyberPixelPro@users.noreply.github.com>
2025-04-07 09:28:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-API_URL = getenv("API_URL", 'https://api.nexgenbots.in') #youtube song url
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.me/RahulTC
 
 UPSTREAM_REPO = getenv(
