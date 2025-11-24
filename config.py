39c461ef79924db650e6a6cfe6317b425ce06110
itzmarwin <itzm4rw1n@gmail.com>
2025-11-24 20:36:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SAMURAIS_NETWORK")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Samurais_Support_Chat")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SamuraisSupportChat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -121,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
